a2d9559d490a792606142db2b02ebd4eb3bbda3e
Ayoub Taouaf <ataouaf@e1r2p1.1337.ma>
2024-02-26 18:35:10 +0100

    Finish first exercise

--- cpp05/ex00/inc/Bureaucrat.hpp
+++ cpp05/ex00/inc/Bureaucrat.hpp
@@ -6,14 +6,45 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:47:00 by ataouaf           #+#    #+#             */
-/*   Updated: 2023/12/06 14:47:40 by ataouaf          ###   ########.fr       */
+/*   Updated: 2024/02/26 18:23:22 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREUCRAT_HPP
 #define BUREUCRAT_HPP
 
+#include <iostream>
+#include <string>
+#include <exception>
 
+class Bureaucrat
+{
+    private:
+        std::string const _name;
+        int _grade;
+    public:
+        Bureaucrat();
+        Bureaucrat(std::string const &name, int grade);
+        Bureaucrat(Bureaucrat const &other);
+        Bureaucrat &operator=(Bureaucrat const &other);
+        ~Bureaucrat();
+        std::string const &getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat);
 
 
 #endif
--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -6,7 +6,30 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 14:46:38 by ataouaf           #+#    #+#             */
-/*   Updated: 2023/12/06 14:46:39 by ataouaf          ###   ########.fr       */
+/*   Updated: 2024/02/26 18:32:48 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/Bureaucrat.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat b1("b1", 1);
+        std::cout << b1 << std::endl;
+        b1.decrementGrade();
+        std::cout << b1 << std::endl;
+        b1.incrementGrade();
+        std::cout << b1 << std::endl;
+        b1.incrementGrade();
+        std::cout << b1 << std::endl;
+        b1.incrementGrade();
+        std::cout << b1 << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return 0;
+}
