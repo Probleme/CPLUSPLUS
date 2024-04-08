a772178ffdc9d064a493afd7f14cc7fbf3433294
Ayoub Taouaf <ataouaf@e1r2p3.1337.ma>
2024-04-08 12:32:03 +0000

    finish rthe first and second exercice the module 06 in cpp

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,70 +6,81 @@
 /*   By: ataouaf <ataouaf@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 13:28:07 by ataouaf           #+#    #+#             */
-/*   Updated: 2024/04/05 16:44:03 by ataouaf          ###   ########.fr       */
+/*   Updated: 2024/04/07 17:38:38 by ataouaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter()
-{
-}
+ScalarConverter::ScalarConverter() {}
 
-ScalarConverter::~ScalarConverter()
-{
-}
+ScalarConverter::~ScalarConverter() {}
 
 void ScalarConverter::convert(const std::string &str)
 {
-    std::cout << "char: ";
-    try
+    if (str == "inff" || str == "-inff" || str == "+inff")
     {
-        int i = std::stoi(str);
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << str << std::endl;
+        std::cout << "double: " << str.substr(0, str.size() - 1) << std::endl;
+        return;
+    }
+    else if (str == "inf" || str == "-inf" || str == "+inf")
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: " << str << "f" << std::endl;
+        std::cout << "double: " << str << std::endl;
+        return;
+    }
+    std::cout << "char: ";
+    std::istringstream iss(str);
+    int i;
+    if (!(iss >> i))
+        std::cout << "impossible" << std::endl;
+    else
+    {
         if (i < 32 || i > 126)
             std::cout << "Non displayable" << std::endl;
         else
             std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cout << "impossible" << std::endl;
-    }
     std::cout << "int: ";
-    try
-    {
-        int i = std::stoi(str);
-        std::cout << i << std::endl;
-    }
-    catch(const std::exception& e)
-    {
+    iss.clear(); // Clear any error flags
+    iss.str(str); // Reset stream with the original input string
+    if (!(iss >> i)) 
         std::cout << "impossible" << std::endl;
-    }
+    else
+        std::cout << i << std::endl;
     std::cout << "float: ";
-    try
-    {
-        float f = std::stof(str);
-        std::cout << f << std::endl;
-        if (f - static_cast<int>(f) == 0)
-            std::cout << f << ".0f" << std::endl;
-        else
-            std::cout << f << "f" << std::endl;
-    }
-    catch(const std::exception& e)
-    {
+    iss.clear();
+    iss.str(str);
+    float f;
+    iss >> f;
+    std::string strValue = iss.str();
+    if (strValue.find("f") != std::string::npos)
+        strValue = strValue.substr(0, strValue.size() - 1);
+    // now we need to clear the stream and reset it with strValue
+    iss.clear();
+    iss.str(strValue);
+    if (!(iss >> f))
         std::cout << "nanf" << std::endl;
-    }
+    else
+        std::cout << f << (f - static_cast<int>(f) == 0 ? ".0f" : "f") << std::endl;
     std::cout << "double: ";
-    try
-    {
-        double d = std::stod(str);
-        if (d - static_cast<int>(d) == 0)
-            std::cout << d << ".0" << std::endl;
-        else
-            std::cout << d << std::endl;
-    }
-    catch(const std::exception& e)
-    {
+    iss.clear();
+    iss.str(str);
+    double d;
+    iss >> d;
+    strValue.clear();
+    strValue = iss.str();
+    if (strValue.find("f") != std::string::npos)
+        strValue = strValue.substr(0, strValue.size() - 1);
+    iss.clear();
+    iss.str(strValue);
+    if (!(iss >> d))
         std::cout << "nan" << std::endl;
-    }
+    else
+        std::cout << d << (d - static_cast<int>(d) == 0 ? ".0" : "") << std::endl;
 }
